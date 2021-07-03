c5562dc36edc527ff8a2ec360ac4ed5b152fe766
Alex Bright <abrighttt@gmail.com>
2021-07-03 18:51:34 -0400

    deployment worked

--- server.js
+++ server.js
@@ -1,44 +1,85 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes');
-const APIRoutes = require('./routes/apiroutes');
-const Store = require('./db/storesClasses');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const database = require("./db/db")
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use('/', htmlRoutes);
-app.use('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 
-app.get('/api/notes', (req, res) => {
-    Store.addNotes(req.body)
-        .then((note) => res.json(note))
-        .then((note) => res.sendFile(path.join(__dirname, './db/db.json')))
-        .catch((err) => {
-            res.status(500).json(err);
-        })
+app.route("/api/notes")
+    
+    .get(function (req, res) {
+        res.json(database);
+    })
+    
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        
+        let highestId = 99;
+        
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                
+                highestId = individualNote.id;
+            }
+        }
+        newNote.id = highestId + 1;
+        
+        database.push(newNote)
+
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        
+        res.json(newNote);
+    });
+
+
+
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            
+            database.splice(i, 1);
+            break;
+        }
+    }
+    
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note has been deleted!");
+        }
+    });
+    res.json(database);
 });
 
-
-
-app.post('/api/notes', (req, res) => {
-    Store.addNotes(req.body)
-        .then((note) => res.json(note))
-        .then((note) => res.sendFile(path.join(__dirname, './db/db.json')))
-        .catch((err) => {
-            res.status(500).json(err);
-        })
-});
-
-
-app.listen(PORT, () => {
-    console.log(`Listening on : ${PORT}`);
-
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
