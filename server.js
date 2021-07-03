089adec30d6a63c4776affc20ee9a69165768e39
Alex Bright <abrighttt@gmail.com>
2021-07-03 00:59:28 -0400

    tried to fix again for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-// const app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const htmlRoutes = require('./routes/htmlRoutes');
